178d48cc18704b77131edff9914c5affa8f49c92
David Wang <dawng88@users.noreply.huggingface.co>
2023-11-04 22:51:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,3 @@ from transformers import pipeline
 text = st.text_area('enter some text:')
 classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
 classifier(text)
-
-
-if text:
-  out = classifer(text)
-  st.json(out)

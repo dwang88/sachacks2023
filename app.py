8099655255218fbe01071d2936a0a5b96b9d18dd
David Wang <dawng88@users.noreply.huggingface.co>
2023-11-04 22:34:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from transformers import pipeline
 
 pipe = pipeline('sentiment-analysis')
-text. st.text_area('enter some text:')
+text = st.text_area('enter some text:')
 
 if text:
   out = pipe(text)
